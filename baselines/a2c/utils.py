8bf5007d0fd76c5b3a6fdceb5b466401c96ace34
Isaac Lascasas <isaaclascasas@gmail.com>
2019-01-28 08:24:58 +0100

    TF Regularization support.

--- baselines/a2c/utils.py
+++ baselines/a2c/utils.py
@@ -53,16 +53,16 @@ def conv(x, scope, *, nf, rf, stride, pad='VALID', init_scale=1.0, data_format='
     nin = x.get_shape()[channel_ax].value
     wshape = [rf, rf, nin, nf]
     with tf.variable_scope(scope):
-        w = tf.get_variable("w", wshape, initializer=ortho_init(init_scale))
-        b = tf.get_variable("b", [1, nf, 1, 1], initializer=tf.constant_initializer(0.0))
+        w = tf.get_variable("w", wshape, initializer=ortho_init(init_scale), collections=[tf.GraphKeys.GLOBAL_VARIABLES, tf.GraphKeys.WEIGHTS])
+        b = tf.get_variable("b", [1, nf, 1, 1], initializer=tf.constant_initializer(0.0), collections=[tf.GraphKeys.GLOBAL_VARIABLES, tf.GraphKeys.BIASES])
         if data_format == 'NHWC': b = tf.reshape(b, bshape)
         return b + tf.nn.conv2d(x, w, strides=strides, padding=pad, data_format=data_format)
 
 def fc(x, scope, nh, *, init_scale=1.0, init_bias=0.0, reuse=None):
     with tf.variable_scope(scope, reuse=reuse):
         nin = x.get_shape()[1].value
-        w = tf.get_variable("w", [nin, nh], initializer=ortho_init(init_scale))
-        b = tf.get_variable("b", [nh], initializer=tf.constant_initializer(init_bias))
+        w = tf.get_variable("w", [nin, nh], initializer=ortho_init(init_scale), collections=[tf.GraphKeys.GLOBAL_VARIABLES, tf.GraphKeys.WEIGHTS])
+        b = tf.get_variable("b", [nh], initializer=tf.constant_initializer(init_bias), collections=[tf.GraphKeys.GLOBAL_VARIABLES, tf.GraphKeys.BIASES])
         return tf.matmul(x, w)+b
 
 def batch_to_seq(h, nbatch, nsteps, flat=False):
@@ -85,9 +85,9 @@ def lstm(xs, ms, s, scope, nh, init_scale=1.0):
     nbatch, nin = [v.value for v in xs[0].get_shape()]
     nsteps = len(xs)
     with tf.variable_scope(scope):
-        wx = tf.get_variable("wx", [nin, nh*4], initializer=ortho_init(init_scale))
-        wh = tf.get_variable("wh", [nh, nh*4], initializer=ortho_init(init_scale))
-        b = tf.get_variable("b", [nh*4], initializer=tf.constant_initializer(0.0))
+        wx = tf.get_variable("wx", [nin, nh*4], initializer=ortho_init(init_scale), collections=[tf.GraphKeys.GLOBAL_VARIABLES, tf.GraphKeys.WEIGHTS])
+        wh = tf.get_variable("wh", [nh, nh*4], initializer=ortho_init(init_scale), collections=[tf.GraphKeys.GLOBAL_VARIABLES, tf.GraphKeys.WEIGHTS])
+        b = tf.get_variable("b", [nh*4], initializer=tf.constant_initializer(0.0), collections=[tf.GraphKeys.GLOBAL_VARIABLES, tf.GraphKeys.BIASES])
 
     c, h = tf.split(axis=1, num_or_size_splits=2, value=s)
     for idx, (x, m) in enumerate(zip(xs, ms)):
@@ -115,15 +115,15 @@ def lnlstm(xs, ms, s, scope, nh, init_scale=1.0):
     nbatch, nin = [v.value for v in xs[0].get_shape()]
     nsteps = len(xs)
     with tf.variable_scope(scope):
-        wx = tf.get_variable("wx", [nin, nh*4], initializer=ortho_init(init_scale))
+        wx = tf.get_variable("wx", [nin, nh*4], initializer=ortho_init(init_scale), collections=[tf.GraphKeys.GLOBAL_VARIABLES, tf.GraphKeys.WEIGHTS])
         gx = tf.get_variable("gx", [nh*4], initializer=tf.constant_initializer(1.0))
         bx = tf.get_variable("bx", [nh*4], initializer=tf.constant_initializer(0.0))
 
-        wh = tf.get_variable("wh", [nh, nh*4], initializer=ortho_init(init_scale))
+        wh = tf.get_variable("wh", [nh, nh*4], initializer=ortho_init(init_scale), collections=[tf.GraphKeys.GLOBAL_VARIABLES, tf.GraphKeys.WEIGHTS])
         gh = tf.get_variable("gh", [nh*4], initializer=tf.constant_initializer(1.0))
         bh = tf.get_variable("bh", [nh*4], initializer=tf.constant_initializer(0.0))
 
-        b = tf.get_variable("b", [nh*4], initializer=tf.constant_initializer(0.0))
+        b = tf.get_variable("b", [nh*4], initializer=tf.constant_initializer(0.0), collections=[tf.GraphKeys.GLOBAL_VARIABLES, tf.GraphKeys.BIASES])
 
         gc = tf.get_variable("gc", [nh], initializer=tf.constant_initializer(1.0))
         bc = tf.get_variable("bc", [nh], initializer=tf.constant_initializer(0.0))
